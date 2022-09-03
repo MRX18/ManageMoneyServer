bab47aba4afc73570fe2641087a5ab27e145d8f7
MRX18 <roma.borisenko18@gmail.com>
2022-09-04 01:10:55 +0300

    added CollectionLengthAttribute

---
+++ ManageMoneyServer/Validations/CollectionLengthAttribute.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.ComponentModel.DataAnnotations;
+
+namespace ManageMoneyServer.Validations
+{
+    public class CollectionLengthAttribute : ValidationAttribute
+    {
+        public readonly int MinLength = 0;
+        public object MaxLength { get; set; }
+        public bool IsRange { get; set; } = true;
+        public CollectionLengthAttribute(int minLength = 0)
+        {
+            MinLength = minLength;
+        }
+        public override string FormatErrorMessage(string name)
+        {
+            int? maxLength = MaxLength as int?;
+            string message = string.Empty;
+
+            if(!string.IsNullOrEmpty(ErrorMessageString)) {
+                if(IsRange)
+                {
+                    if(maxLength.HasValue)
+                    {
+                        message = string.Format(ErrorMessageString, name, maxLength.Value + 1, MinLength - 1);
+                    } else
+                    {
+                        message = string.Format(ErrorMessageString, name, MinLength - 1);
+                    }
+                } else
+                {
+                    message = string.Format(ErrorMessageString, name, MinLength);
+                }
+            }
+
+            return message;
+        }
+        public override bool IsValid(object? value)
+        {
+            int? maxLength = MaxLength as int?;
+            if (value != null)
+            {
+                IList collection = value as IList;
+
+                if(IsRange)
+                {
+                    bool result = true;
+
+                    if (maxLength.HasValue)
+                        result = collection.Count <= maxLength.Value;
+
+                    return result && collection.Count >= MinLength;
+                } else
+                {
+                    if (collection.Count == MinLength)
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            return false;
+        }
+    }
+}
